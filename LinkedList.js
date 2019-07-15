1bb17460481ea1c88f675b13ccb24fd246a88b77
emmanuel aubrey <emmanuelthegrey@gmail.com>
2019-07-15 11:08:03 -0400

    LinkedList Skeleton

---
+++ LinkedList.js
@@ -0,0 +1,25 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(value){
+        this.head = {
+            value: value, 
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value){
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+}
