b34cfd60e5533a9e0e11f50b0ddb938c0b57ba21
emmanuel aubrey <emmanuelthegrey@gmail.com>
2019-07-16 09:24:39 -0400

    add pop method

--- MyArray.js
+++ MyArray.js
@@ -5,15 +5,28 @@ class MyArray{
     }
 
     get(index){
-        return this.data[index];
+        if(this.data[index])
+            return this.data[index];
     }
 
     push(item){
         this.data[this.length] = item;
         this.length++;
     }
+
+    pop(){
+        if(this.length > 0){
+            this.data[this.length - 1] = null;
+            this.length--;
+        }
+       
+    }
     
 
 }
 
 const newArray = new MyArray();
+newArray.push(1);
+console.log(newArray);
+newArray.pop()
+console.log(newArray);
