4ae110ebe79fbd7d49ba365168b8ddf9e61242a7
emmanuel aubrey <emmanuelthegrey@gmail.com>
2019-07-17 08:33:55 -0400

    Fixed push method. Added pop method

--- Stack.js
+++ Stack.js
@@ -1,43 +1,49 @@
-class Node{
-    constructor(value){
+class Node {
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
 
-    class Stack{
-        constructor(){
-            this.top = null;
-            this.bottom = null;
-            this.length = 0;
-        }
-        peek(){
-            return this.top;
-        }
-        push(value){
-            let newNode = new Node(value);
-            if(this.length === 0){
-                this.top = newNode;
-                this.bottom = newNode;
-                this.bottom.next = this.top;
-            }else{
-                this.top.next = newNode;
-                this.top = newNode;
-            }
-            this.length++;
-            return this;
-        }
-        pop(){
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    peek() {
+        return this.top;
+    }
+    push(value) {
+        let newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+            this.top.next = this.bottom;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
 
         }
-        isEmpty(){
-
+        this.length++;
+        return this;
+    }
+    pop() {
+        if (this.length > 0) {
+            var nodeToReturn = new Node(this.top.value);
+            this.top = this.top.next;
         }
     }
+    isEmpty() {
 
-    const myStack = new Stack();
-    myStack.push("Udemy");
-    myStack.push("Youtube");
-    myStack.push("Google");
-    console.log(myStack.peek().value);
-    
+    }
+}
+
+const myStack = new Stack();
+myStack.push("Udemy");
+myStack.push("Youtube");
+myStack.push("Google");
+console.log(myStack.peek().value);
+myStack.pop();
+console.log(myStack.peek().value);
+console.log();    
