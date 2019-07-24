c3f4da422f27bad0d99008619fe7bbd3b8918c58
emmanuel aubrey <emmanuelthegrey@gmail.com>
2019-07-24 19:41:26 -0400

    insert function skeleton and travese

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -11,7 +11,28 @@ class BinarySearchTree{
         this.root = null;
     }
     insert(value){
+      const newNode = new Node(value);
+      if(this.root === null) this.root = value;
+      else{
+          let currentNode = this.root;
 
+          while(true){
+              if(value < currentNode.value){
+                  if(!currentNode.left){
+                      currentNode.left = newNode;
+                      return this;
+                  }
+                  currentNode = currentNode.left
+              }
+              else{
+                if(!currentNode.right){
+                    currentNode.right = newNode;
+                    return this;
+                }
+                currentNode = currentNode.right
+            }
+          }
+      }
     }
     lookup(value){
 
@@ -20,3 +41,20 @@ class BinarySearchTree{
         
     }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+JSON.stringify(traverse(tree.root))
+
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+  }
